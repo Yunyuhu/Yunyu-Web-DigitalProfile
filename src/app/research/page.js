bf806db706afd3f36c8275844b204cfd1db752bd
Yunyuhu <55233573+Yunyuhu@users.noreply.github.com>
2025-04-19 04:10:15 +0800

    word

--- src/app/research/page.js
+++ src/app/research/page.js
@@ -44,7 +44,7 @@ export default function Research() {
         <div className="flex flex-col w-full">
           <h1 className="text-2xl font-bold">研究與活動 / Research and Activities</h1>
           <h3 className="text-sm text-gray-600 mt-2">
-          探索科技與設計交會的邊界，在不同場域和切角中實踐。探索科技與設計交會的邊界，在不同場域和切角中實踐。
+          探索科技與設計交會的邊界，在不同場域和切角中實踐。
           </h3>
           <div className="w-full h-px bg-gray-200 mt-4" />
         </div>
